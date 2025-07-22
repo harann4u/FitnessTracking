444d62e96aab0c0d77e0a9cefc7983d8dd980f5f
harann4u <harann4u@gmail.com>
2025-07-22 14:59:47 +0530

    Performance Checking

--- client/src/pages/mainLayout.tsx
+++ client/src/pages/mainLayout.tsx
@@ -2,12 +2,8 @@ import { HeaderStyled, MainBoxPannelStyled, MainPageWrapperStyled, MainPannelSty
 import SidePannel from "./dashboard/sidePannel/sidePannel"
 import DashboardLaout from "./dashboard/dashboardLayout"
 import MainHeader from "./mainHeader/mainHeader"
-//  import Blogging from "./blogging/blogging"
-// import ProfileSetting from "./profileSetting/profileSetting"
 import { useSelector } from "react-redux"
 import type { RootState } from "../store/store"
-// import WorkoutTracking from "./workoutTracking/workoutTracking"
-// import GoalSetting from "./goalSetting/goalSetting"
 import type { tabJsonState } from "../store/slice/tabJsonSlice"
 import { lazy, Suspense } from "react"
 
@@ -28,13 +24,6 @@ const MainLayout = () => {
             </HeaderStyled>
             <MainPannelStyled>
                 <SidePannel />
-                  {/* <MainBoxPannelStyled>
-                    {tab[0].id == 0 && <DashboardLaout/> }
-                    {tab[0].id == 2 && <WorkoutTracking/>}
-                    {tab[0].id == 1 && <ProfileSetting/>}
-                    {tab[0].id == 3 && <GoalSetting/>}
-                    {tab[0].id == 4 && <Blogging/>}
-                </MainBoxPannelStyled> */}
                 <MainBoxPannelStyled>
                     {tab[0].id == 0 && <DashboardLaout />}
                     {tab[0].id == 2 && <Suspense fallback={<div>...Loading Bloggings</div>}><LazyworkoutTracking /></Suspense>}
