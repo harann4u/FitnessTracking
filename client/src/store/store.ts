79900ec928807c3515fec8724d4c48a97649548d
harann4u <harann4u@gmail.com>
2025-07-23 10:06:38 +0530

    middleware Logger Implementatation

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,14 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  tabSelectionReducer  from "./slice/tabJsonSlice";
+import tabSelectionReducer from "./slice/tabJsonSlice";
 import loginInfoReducer from "./slice/loginInfo/loginInfoSlice";
 import userDeatailsReducer from "./slice/userDetails/userDetailsSlice"
+import { createLogger } from "redux-logger";
+
+
+const customLogger = createLogger({
+    collapsed: true,
+    diff: true,
+    duration: true,
+})
 
 export const store = configureStore({
-    reducer:{
-        tab:tabSelectionReducer,
-        loginInfo:loginInfoReducer,
-        userDetails:userDeatailsReducer,
-    }
+    reducer: {
+        tab: tabSelectionReducer,
+        loginInfo: loginInfoReducer,
+        userDetails: userDeatailsReducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(customLogger)
 })
 
 
